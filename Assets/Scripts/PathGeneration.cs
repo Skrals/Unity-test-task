c91f6b98aa578c0bb2e2121e7ede68b28d6675dd
Ivan <39524682+Skrals@users.noreply.github.com>
2021-05-09 09:24:40 +0300

    Path generator update

--- Assets/Scripts/PathGeneration.cs
+++ Assets/Scripts/PathGeneration.cs
@@ -21,96 +21,100 @@ public class PathGeneration : MonoBehaviour
     private float randomPositionY;
     private float randomPositionZ;
     private bool genReady;
+    private int currentElementsNumber;
     private void Start()
     {
+        currentElementsNumber = pathElements.Length;
         genReady = false;
     }
     private void Update()
     {
-        
+        if (currentElementsNumber != pathElements.Length)
+        {
+            genReady = false;
+            Delete();
+        }
+        if (genReady == false && pathElements.Length >= 2)
+        {
+            genReady = true;
+            Generation();
+        }
     }
     private void Generation()
     {
-        if (pathElements.Length >= 2)
+        for (int i = 0; i <= pathElements.Length - 1; i++)
         {
-            for (int i = 0; i <= pathElements.Length-1; i++)
-            {
-                PointRandomization();
-                Vector3 newPosition = new Vector3(randomPositionX, randomPositionY, randomPositionZ);
-                this.generationObject.transform.localPosition = newPosition;
-                var element =  Instantiate(generationObject, generationObject.transform.position, generationObject.transform.rotation);
-                pathElements[i] = element.transform;
-            }
+            PointRandomization();
+            Vector3 newPosition = new Vector3(randomPositionX, randomPositionY, randomPositionZ);
+            this.generationObject.transform.localPosition = newPosition;
+            var element = Instantiate(generationObject, generationObject.transform.position, generationObject.transform.rotation);
+            pathElements[i] = element.transform;
         }
+        currentElementsNumber = pathElements.Length;
     }
-    private void PointRandomization ()
+    private void PointRandomization()
     {
-        randomPositionX = Random.Range(minRandomRange,maxRandomRange);
+        randomPositionX = Random.Range(minRandomRange, maxRandomRange);
         randomPositionY = Random.Range(minRandomRange, maxRandomRange);
         randomPositionZ = Random.Range(minRandomRange, maxRandomRange);
     }
     private void Delete()
     {
-
+        GameObject[] objectsForDelete = GameObject.FindGameObjectsWithTag("Point");
+        foreach (GameObject g in objectsForDelete)
+        {
+            Destroy(g);
+        }
     }
-
     public void OnDrawGizmos()
     {
-        if(pathElements == null || pathElements.Length <2)
+        if (pathElements == null || pathElements.Length < 2)
         {
             return;
-        }
-        else
-        {
-            if (genReady == false)
-            {
-                genReady = true;
-                Generation();
-            }
         }
         for (var i = 1; i < pathElements.Length; i++)
         {
             Gizmos.DrawLine(pathElements[i - 1].position, pathElements[i].position);
         }
-        if(pathType == PathTypes.loop)
+        if (pathType == PathTypes.loop)
         {
             Gizmos.DrawLine(pathElements[0].position, pathElements[pathElements.Length - 1].position);
         }
     }
-    public IEnumerator<Transform> GetNextPathPoint ()
+    public IEnumerator<Transform> GetNextPathPoint()
     {
-        if(pathElements == null || pathElements.Length <1)
+        if (pathElements == null || pathElements.Length < 1)
         {
             yield break;
         }
-        while(true)
+        while (true)
         {
             yield return pathElements[moveTo];
 
-            if(pathElements.Length == 1)
+            if (pathElements.Length == 1)
             {
                 continue;
             }
 
-            if(pathType == PathTypes.linear)
+            if (pathType == PathTypes.linear)
             {
-                if(moveTo <=0)
+                if (moveTo <= 0)
                 {
                     movementDirection = 1;
                 }
-                else if( moveTo >= pathElements.Length -1)
+                else if (moveTo >= pathElements.Length - 1)
                 {
                     movementDirection = -1;
                 }
             }
             moveTo = moveTo + movementDirection;
-            if(pathType == PathTypes.loop)
+            if (pathType == PathTypes.loop)
             {
-                if(moveTo >= pathElements.Length)
+                if (moveTo >= pathElements.Length)
                 {
                     moveTo = 0;
                 }
-                if(moveTo <0)
+                if (moveTo < 0)
                 {
                     moveTo = pathElements.Length - 1;
                 }
